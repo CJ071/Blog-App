c8451998e8d9a7b893cfe582896ab1e6152f1071
CJ071 <anomosc@gmail.com>
2024-05-07 14:09:17 +0500

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import env from 'dotenv'
+
+env.config()
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>console.log('Succcessfully connected to database'))
 
 const app=express()
 
