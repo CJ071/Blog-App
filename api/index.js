7cb9b237ee44d62c11cdcaeeb90c03717ee5a606
CJ071 <anomosc@gmail.com>
2024-06-05 20:52:35 +0500

    add pre-installation

--- api/index.js
+++ api/index.js
@@ -6,12 +6,15 @@ import authRoute from './routes/auth.route.js'
 import postRoute from './routes/post.route.js'
 import commentRoute from './routes/comment.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 env.config()
 
 mongoose
 .connect(process.env.MONGO)
 .then(()=>console.log('Succcessfully connected to database'))
+
+const __dirname=path.resolve()
 
 const app=express()
 
@@ -24,6 +27,12 @@ app.use('/api/user',userRoute)
 app.use('/api/auth',authRoute)
 app.use('/api/post',postRoute)
 app.use('/api/comment',commentRoute)
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500
